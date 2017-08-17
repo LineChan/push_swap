0cb5ee4fff5d919ddc9a29953529357c7e3a0a1d
LineChan <mailine@gmail.com>
2017-08-17 20:45:24 +0200

    merge all repo

--- srcs/checker/srcs/ft_check_instruct.c
+++ srcs/checker/srcs/ft_check_instruct.c
@@ -8,11 +8,7 @@ void			ft_check_instruct(void)
 {
 	char	*line;
 
-<<<<<<< HEAD
-	while ((line = ft_gnt(0, '\n')) && ft_strcmp("",line))
-=======
 	while ((line = ft_gnt(0, '\n')) && ft_strcmp("", line))
->>>>>>> dc63d21fa8b1fdeb377dd67bf2050b5508a3eddb
 	{
 		if (!ft_check_do_instruct(line))
 			EXIT_FAIL("Wrong instruction");
--- srcs/checker/srcs/ft_curse_print_stack.c
+++ srcs/checker/srcs/ft_curse_print_stack.c
@@ -3,20 +3,12 @@
 /*
 ** Prints stacks with ncurse
 */
-void			ft_curse_print_stack(void)
+static void			ft_curse_stack_a(int parent_y, int parent_x)
 {
-	int		parent_x;
-	int		parent_y;
 	int		x;
 	int		y;
 	node	it;
 
-	x = 2;
-	getmaxyx(CURSE, parent_y, parent_x);
-	mvwprintw(CURSE, (parent_y >> 1) - 3, x, "Stack A");
-	wattron(CURSE, COLOR_PAIR(2));
-	wprintw(CURSE, "  ---> ");
-	wattroff(CURSE, COLOR_PAIR(2));
 	it = HEAD_A.next;
 	x = 17;
 	y = parent_y;
@@ -35,14 +27,17 @@ void			ft_curse_print_stack(void)
 		}
 		it = it->next;
 	}
-	x = 2;
-	y = parent_y;
-	mvwprintw(CURSE, (y >> 1) + 2, x, "Stack B");
-	wattron(CURSE, COLOR_PAIR(2));
-	wprintw(CURSE, "  ---> ");
-	wattroff(CURSE, COLOR_PAIR(2));
+}
+
+static void			ft_curse_stack_b(int parent_y, int parent_x)
+{
+	node	it;
+	int		y;
+	int		x;
+
 	it = HEAD_B.next;
 	x = 17;
+	y = parent_y;
 	while (it != &HEAD_B)
 	{
 		(TOP_B == it) ? wattron(CURSE, COLOR_PAIR(5)) : 0;
@@ -58,6 +53,26 @@ void			ft_curse_print_stack(void)
 		}
 		it = it->next;
 	}
+}
+
+void			ft_curse_print_stack(void)
+{
+	int		parent_x;
+	int		parent_y;
+	node	it;
+
+	getmaxyx(CURSE, parent_y, parent_x);
+	mvwprintw(CURSE, (parent_y >> 1) - 3, 2, "Stack A");
+	wattron(CURSE, COLOR_PAIR(2));
+	wprintw(CURSE, "  ---> ");
+	wattroff(CURSE, COLOR_PAIR(2));
+	it = HEAD_A.next;
+	ft_curse_stack_a(parent_y, parent_x);
+	mvwprintw(CURSE, (parent_y >> 1) + 2, 2, "Stack B");
+	wattron(CURSE, COLOR_PAIR(2));
+	wprintw(CURSE, "  ---> ");
+	wattroff(CURSE, COLOR_PAIR(2));
+	ft_curse_stack_b(parent_y, parent_x);
 	refresh();
 	wrefresh(CURSE);
 }
