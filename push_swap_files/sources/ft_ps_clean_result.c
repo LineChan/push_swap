e6f0aa6114bf5fbb06a0ef85fb37fbef81d1788f
mvillemi <mvillemi@student.42.fr>
2017-10-10 17:07:42 +0200

    free in clean result

--- push_swap_files/sources/ft_ps_clean_result.c
+++ push_swap_files/sources/ft_ps_clean_result.c
@@ -6,7 +6,7 @@
 /*   By: mvillemi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/04 16:07:48 by mvillemi          #+#    #+#             */
-/*   Updated: 2017/10/04 17:40:45 by mvillemi         ###   ########.fr       */
+/*   Updated: 2017/10/10 17:02:15 by mvillemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,8 @@ static void		ft_ps_clean_push(t_lst *it)
 		return ;
 	if (NEXT(it, "pa", "pb") || NEXT(it, "pb", "pa"))
 	{
-		ft_lst_del(it);
 		ft_lst_del(it->next);
-		NB_MOVE -= 2;
+		ft_lst_del(it);
 	}
 }
 
@@ -35,16 +34,18 @@ static void		ft_ps_clean_rotate(t_lst *it)
 	if (NEXT(it, "ra", "rra") || NEXT(it, "rra", "ra") ||
 			NEXT(it, "rb", "rrb") || NEXT(it, "rrb", "rb"))
 	{
-		ft_lst_del(it);
 		ft_lst_del(it->next);
-		NB_MOVE -= 2;
+		ft_lst_del(it);
 	}
 }
 
-static void		ft_ps_del_node(t_lst *it, char *move)
+static void		ft_ps_del_node(t_lst *it)
 {
-	MOVE(it) = move;
+	t_lst	*ptr;
+
+	ptr = it;
 	ft_lst_del(it);
+	ft_memdel((void **)&ptr);
 	--NB_MOVE;
 }
 
@@ -53,16 +54,24 @@ static void		ft_ps_clean_double(t_lst *it)
 	if ((it == &INFO) || (it->next == &INFO))
 		return ;
 	if (NEXT(it, "ra", "rb") || NEXT(it, "rb", "ra"))
-		ft_ps_del_node(it->next, "rr");
+	{
+		ft_strncpy(MOVE(it), "rr", 2);
+		ft_ps_del_node(it->next);
+	}
 	if (NEXT(it, "rra", "rrb") || NEXT(it, "rrb", "rra"))
-		ft_ps_del_node(it->next, "rrr");
+	{
+		ft_strncpy(MOVE(it), "rrr", 3);
+		ft_ps_del_node(it->next);
+	}
 	if (NEXT(it, "sa", "sb") || NEXT(it, "sb", "sa"))
-		ft_ps_del_node(it->next, "ss");
+	{
+		ft_strncpy(MOVE(it), "ss", 2);
+		ft_ps_del_node(it->next);
+	}
 	if (NEXT(it, "rr", "rrr") || NEXT(it, "rrr", "rr"))
 	{
-		ft_lst_del(it);
 		ft_lst_del(it->next);
-		NB_MOVE -= 2;
+		ft_lst_del(it);
 	}
 }
 
